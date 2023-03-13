cee9f8f1d8ec663063b9637e558f2e57ad52549b
AlbertoAchegaLeitão <albertoleitao94@gmail.com>
2023-03-13 17:14:09 +0000

    inicio do build

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import About from "../components/About";
 import Contact from "../components/Contact";
+import { basename } from "path";
 
 const routes = () => {
   return (
